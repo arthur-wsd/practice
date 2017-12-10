39e32082a9c86fc15bc6feac219dd6769ee517ad
gordon043 <603472464@qq.com>
2017-12-10 19:15:51 +0800

    项目初始化

---
+++ src/main/java/demo/gordon/helloworld/App.java
@@ -0,0 +1,10 @@
+package demo.gordon.helloworld;
+
+/**
+ * Created by chenguolin on 2017/12/10.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("this is helloworld app.");
+    }
+}
